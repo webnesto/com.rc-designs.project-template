2470bb42fc604066a0a3df526d14dc8a33f6644a
ernesto johnson <nesto@rc-designs.com>
2012-04-10 19:37:48 -0700

    * moved "build" -> "scripts" * added "build" for build config files

--- www/scripts/torso/Router.js
+++ www/scripts/torso/Router.js
@@ -1,50 +1,9 @@
 define( function(){
-
-	// simplified $.deparam analog
-	// function deparam (paramString){
-		// var result = {};
-		// if( ! paramString){
-			// return result;
-		// }
-		// $.each(paramString.split('&'), function(index, value){
-			// if(value){
-				// var param = value.split('=');
-				// result[param[0]] = param[1];
-			// }
-		// });
-		// return result;
-	// };
-
-
-	// var namedParam	= /=\w+/g;
-	// var splatParam	= /\*\w+/g;
-	// var escapeRegExp  = /[-[\]{}()+?.,\\^$|#\s]/g;
-
 	return Backbone.Router.extend( {
 		_extractParameters: function( route, fragment ) {
 			var _intended = fragment.split( "&" )[ 0 ];
 			var result = route.exec( _intended ).slice( 1 );
 			return result;
 		}
-		// _extractParameters: function( route, fragment ) {
-			// // log.debug( route, fragment );
-			// // var _intended = fragment.split( "&" )[ 0 ];
-			// log.debug( route.exec( fragment ) );
-			// var result = route.exec( fragment ).slice( 1 );
-			// return result;
-			// // result.unshift( deparam( result[ result.length-1 ] ) );
-			// // return result.slice( 0, -1 );
-		// }
-	// ,
-		// _routeToRegExp: function( route ){
-			// log.debug( "_routeToRegExp in: ", route );
-			// route = route
-				// .replace(escapeRegExp, '\\$&')
-				// .replace(namedParam, '=([^&]+)&?.*')
-				// .replace(splatParam, '(.*?)')
-			// ;
-			// log.debug( "_routeToRegExp out: ", route );
-			// return new RegExp( route );
-		// }
 	} );
 } );
